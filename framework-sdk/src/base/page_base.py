c8c77106fafcb37701519ea2100eaa87d8176f16
Krikaliov <hjvsvminecraft@gmail.com>
2026-02-10 22:38:31 +0100

    Added some comments to make the code a little bit clearer

--- framework-sdk/src/base/page_base.py
+++ framework-sdk/src/base/page_base.py
@@ -1,11 +1,27 @@
 from uuid import uuid4
 from object import AbstractClass
 
+# - - - - - - - - - - - - - - - - -
+# Page base skeleton abstract class
+# - - - - - - - - - - - - - - - - -
+# All pages has those attributes and methods.
 class PageBase:
   def __init__(self, path:str):
-    self.key:str = f'{uuid4()}'
+    # Relative path of the page from the app web root
     self.path:str = path
+
+    # Last build UUID registered. Empty when initialized.
+    # When the app builder computes references, it generates
+    # a random UUID called "build UUID". Every page it computes
+    # ahead, it compares this generated UUID with the page's one.
+    # If they differs, this means that this page was still not built.
+    # Thus, the app builder push this page in the stack of the
+    # remaining pages meant to be built and assignes its generated
+    # UUID to this attribute to prevent this page from being
+    # built more than once.
+    self.last_build_uuid:str = ''
+  
+  # Build function.
+  # This write the built data in the file 'index.html'.
   def build(self) -> None:
     raise AbstractClass()
-  def is_same(self, other:PageBase):
-    return self.key == other.key
--- framework-sdk/src/core/reference.py
+++ framework-sdk/src/core/reference.py
@@ -1,5 +1,10 @@
 from ..base.page_base import PageBase
 
+# - - - - - - - -
+# Reference class
+# - - - - - - - -
+# This object refers either to an existing page
+# of the web app or an external link.            
 class Reference:
   def __init__(self, path:str, external:bool = False):
     self.path:str = path
